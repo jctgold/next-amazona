1b3a98d3ae89e932fc073b9ac07c218c36d00101
jctgold <74652116+jctgold@users.noreply.github.com>
2022-07-22 17:33:28 +0800

    login function

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { Store } from '../utils/Store';
+import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default function Layout({ title, children }) {
-  const { state, dispatch } = useContext(Store);
+  const { status, data: session } = useSession();
+
+  const { state } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
 
@@ -18,6 +23,8 @@ export default function Layout({ title, children }) {
         <meta name="description" content="Ecommerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
 
       <div className="flex min-h-screen flex-col justify-between">
         <header>
@@ -37,9 +44,15 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link href="/login">
-                <a className="p-2">Login</a>
-              </Link>
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
